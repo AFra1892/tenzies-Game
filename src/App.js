7b9090dfb427eac9e694a1ff690609cc300171d2
amir <mashhadifarahani1@gmail.com>
2023-09-30 22:43:22 +0330

    adding roll function

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React from "react";
 import Die from './Die'
 
 export default function App(){
+  
+  const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        setDice(allNewDice())
+    }
+  const dieElement = dice.map(die => <Die value={die}/>)
   return(
     <main>
-     <div className="dice-container">
-                <Die value="1" />
-                <Die value="2" />
-                <Die value="3" />
-                <Die value="4" />
-                <Die value="5" />
-                <Die value="6" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-            </div>
+      <div className="dice-container">
+          {dieElement}
+      </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   )
 }
